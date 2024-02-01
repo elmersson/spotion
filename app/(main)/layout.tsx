fc56781af1d91f1c0c115d05b8f4a24d7da0d5d5
elmersson <elmerssonrasmus@gmail.com>
2024-02-01 07:15:32 +0100

    feat: layout (#5)

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { redirect } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { ReactNode } from 'react';
+
+import { Spinner } from '@/components/spinner';
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
+  const session = useSession();
+
+  if (session.status === 'loading') {
+    <div className='flex h-full items-center justify-center'>
+      <Spinner size='lg' />
+    </div>;
+  }
+
+  if (!session || session.status !== 'authenticated') {
+    return redirect('/');
+  }
+
+  return (
+    <div className='flex h-full dark:bg-[#1F1F1F]'>
+      <main className='h-full flex-1 overflow-y-auto'>{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
