6b962c37a7661306e44a8720f3e4626fea3e5564
RasmusElmersson <rasmus.elmersson@martinservera.se>
2024-02-11 08:45:48 +0100

    feat: fetch library items and disply in accordian

---
+++ app/(authenticated)/_components/user-actions.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Plus, Search, Settings } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
+import { GoHome, GoHomeFill } from 'react-icons/go';
+import { PiBrowsersDuotone, PiBrowsersFill } from 'react-icons/pi';
+
+import { useSearch } from '@/hooks/use-search';
+import { useSettings } from '@/hooks/use-settings';
+import { cn } from '@/lib/utils';
+
+export const UserActions = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const settings = useSettings();
+  const search = useSearch();
+
+  const isHome = pathname === '/';
+  const HomeIcon = isHome ? GoHomeFill : GoHome;
+  const isBrowse = pathname === '/browse';
+  const BrowseIcon = isBrowse ? PiBrowsersFill : PiBrowsersDuotone;
+  return (
+    <div className='pl-3'>
+      <div
+        onClick={() => router.push('/')}
+        className={cn(
+          'group flex min-h-[27px] w-full items-center py-1 pr-3 text-sm font-medium text-muted-foreground hover:bg-primary/5 hover:text-white',
+          isHome && 'text-white'
+        )}
+      >
+        <HomeIcon className='mr-2 shrink-0' size={16} />
+        <span className='line-clamp-1'>Home</span>
+      </div>
+      <div
+        onClick={search.onOpen}
+        className='group flex min-h-[27px] w-full items-center py-1 pr-3 text-sm font-medium text-muted-foreground hover:bg-primary/5 hover:text-white'
+      >
+        <Search className='mr-2 shrink-0' size={16} />
+        <span className='line-clamp-1'>Search</span>
+      </div>
+      <div
+        onClick={() => router.push('/browse')}
+        className={cn(
+          'group flex min-h-[27px] w-full items-center py-1 pr-3 text-sm font-medium text-muted-foreground hover:bg-primary/5 hover:text-white',
+          isBrowse && 'text-white'
+        )}
+      >
+        <BrowseIcon className='mr-2 shrink-0' size={16} />
+        <span className='line-clamp-1'>Browse</span>
+      </div>
+      <div
+        onClick={settings.onOpen}
+        className='group flex min-h-[27px] w-full items-center py-1 pr-3 text-sm font-medium text-muted-foreground hover:bg-primary/5 hover:text-white'
+      >
+        <Settings className='mr-2 shrink-0' size={16} />
+        <span className='line-clamp-1'>Settings</span>
+      </div>
+      <div className='group flex min-h-[27px] w-full items-center py-1 pr-3 text-sm font-medium text-muted-foreground hover:bg-primary/5 hover:text-white'>
+        <Plus className='mr-2 shrink-0' size={16} />
+        <span className='truncate'>Create playlist</span>
+      </div>
+    </div>
+  );
+};
--- types/types.ts
+++ types/types.ts
@@ -107,3 +107,38 @@ export interface TrackAnalysis {
   tempo: number;
   valence: number;
 }
+
+export interface Show {
+  available_markets: string[];
+  copyrights: { text: string; type: string }[];
+  description: string;
+  html_description: string;
+  explicit: boolean;
+  external_urls: { spotify: string };
+  href: string;
+  id: string;
+  images: Image[];
+  is_externally_hosted: boolean;
+  languages: string[];
+  media_type: string;
+  name: string;
+  publisher: string;
+  type: 'show';
+  uri: string;
+  total_episodes: number;
+}
+
+export interface UserSavedShow {
+  added_at: string;
+  show: Show;
+}
+
+export interface UserSavedShowsResponse {
+  href: string;
+  limit: number;
+  next: string | null;
+  offset: number;
+  previous: string | null;
+  total: number;
+  items: UserSavedShow[];
+}
